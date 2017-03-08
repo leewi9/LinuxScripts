f8ab5bf5914ce6a08b91fc6bd233db43aae823b9
leewi9 <leewi9@yahoo.com>
2017-03-08 16:43:09 +0800

    create file

--- install.sh
+++ install.sh
@@ -1,2 +1,18 @@
-mv ~/.vimrc .
-ln -s .vimrc ~/
+#!/bin/bash
+
+file="$HOME/.vimrc"
+
+#echo $HOME
+#echo $file
+
+if [ -f "$file" ]
+then
+    #echo "$file already exists!"
+    mv "$HOME/.vimrc" "$HOME/.vimrc.bak"
+    rm -f "$HOME/.vimrc"
+else
+    #echo "$file not found, create link..."
+fi
+
+
+ln -s "$PWD/.vimrc" "$HOME/"
