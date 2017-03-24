f9a01d946b92c596e2fc98517fb865593e0f1d72
leewi9 <leewi9@yahoo.com>
2017-03-24 21:33:49 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -32,7 +32,7 @@ then
     #echo "Vundle already set!"
     echo
 else
-    sudo git clone "https://github.com/VundleVim/Vundle.vim.git" "$HOME/.vim/bundle/Vundle.vim"
+    git clone "https://github.com/VundleVim/Vundle.vim.git" "$HOME/.vim/bundle/Vundle.vim"
 fi
 
 if [ -s "$HOME/.vimrc" ]
