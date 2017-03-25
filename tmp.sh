43ce1fd621e769721ca3aaa577bf8d67aba31450
leewi9 <leewi9@yahoo.com>
2017-03-25 15:37:50 +0800

    Create tmp.sh

---
+++ tmp.sh
@@ -0,0 +1,27 @@
+#
+adduser leewi9              # 需要设置密码、个人信息等
+usermod -a -G sudo leewi9   # 加入sudo
+su leewi9
+cd ~
+
+#
+mkdir ATOOLS
+mkdir AONLINE
+mkdir ALOGS
+
+
+# 修改hostname
+sudo vim /etc/hostname
+sudo vim /etc/hosts      # 例如 127.0.1.1       linode2845632-Ubuntu.members.linode.com   linode2845632-Ubuntu
+
+
+# 设置
+ssh-keygen  # 按默认提示回车
+touch ~/.ssh/authorized_keys
+ls -la
+cd .ssh
+ls -la
+cat id_rsa.pub >> authorized_keys
+chmod 700 ~/.ssh
+chmod 600 ~/.ssh/authorized_keys
+cat id_rsa # 再使用puttygen转换
