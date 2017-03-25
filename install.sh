e41a0e7a477d18e99c343b935fdba21014a64f24
leewi9 <leewi9@yahoo.com>
2017-03-25 16:24:19 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -65,6 +65,8 @@ fi
 
 ln -s "$PWD/.vimrc" "$HOME/"
 
+sudo vim -c "PluginInstall" -c "q" -c "q"
+
 ##### 处理用户配置文件 #####
 cat "$PWD/.bashrc_add" >> "$HOME/.bashrc"   # TODO 避免多次执行
 source "$HOME/.bashrc"
