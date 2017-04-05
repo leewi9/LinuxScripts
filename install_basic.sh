76272e9b314c0b794901bc24b69f00c7f82e0650
leewi9 <leewi9@yahoo.com>
2017-04-05 14:38:39 +0800

    Update and rename install.sh to install_basic.sh

--- install_basic.sh
+++ install_basic.sh
@@ -19,31 +19,12 @@ sudo locale-gen en_US.UTF-8
 sudo update-locale LC_ALL="en_US.UTF-8" LANG="en_US.UTF-8" LANGUAGE="en_US:en"
 
 #
-sudo LC_ALL=C.UTF-8 add-apt-repository ppa:ondrej/php -y
-sudo apt-add-repository ppa:nginx/stable -y
 sudo apt-get -y update && sudo apt-get -y upgrade
 
-# 安装 php5.6
-echo "----> start installing php5.6 ..."
-sudo apt-get -y install php5.6 php5.6-cli php5.6-fpm php5.6-mysql php5.6-gd php5.6-curl php5.6-mbstring php5.6-dom php5-xdebug php5.6-zip php5.6-bz2 php5.6-json php5.6-opcache php5.6-readline
-
 #####  #####
-sudo apt-get install -y nginx git vim zip htop wget
+sudo apt-get install -y git vim zip htop wget
 sudo apt-get install -y build-essential
 sudo apt-get install debian-goodies  # dpigs -H
-
-# 安装 composer
-echo "----> start installing composer ..."
-sudo curl -sS https://getcomposer.org/installer | sudo php
-sudo mv composer.phar /usr/bin/composer
-sudo rm -f composer.phar 
-
-# 安装 mysql5.6
-echo "----> start installing mysql5.6 ..."
-sudo apt-get -y install debconf-utils
-sudo debconf-set-selections <<< 'mysql-server-5.6 mysql-server/root_password password MYSQL_20170324_!@#'  # 设置mysql的root密码！！
-sudo debconf-set-selections <<< 'mysql-server-5.6 mysql-server/root_password_again password MYSQL_20170324_!@#'
-sudo apt-get -y install mysql-server-5.6 mysql-client-5.6
 
 ##### 处理vim配置文件 #####
 if [ -s "$HOME/.vim/bundle/Vundle.vim" ]
