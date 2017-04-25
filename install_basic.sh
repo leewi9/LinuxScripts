5ada56da5238178d26219f9999eda3c2eb1266b2
leewi9 <leewi9@yahoo.com>
2017-04-26 01:05:18 +0800

    Update install_basic.sh

--- install_basic.sh
+++ install_basic.sh
@@ -22,7 +22,7 @@ sudo update-locale LC_ALL="en_US.UTF-8" LANG="en_US.UTF-8" LANGUAGE="en_US:en"
 sudo apt-get -y update && sudo apt-get -y upgrade
 
 #####  #####
-sudo apt-get install -y git vim zip htop wget
+sudo apt-get install -y git vim zip htop wget tree
 sudo apt-get install -y supervisor
 sudo apt-get install -y build-essential
 sudo apt-get install -y debian-goodies  # dpigs -H
