f8c5c0a33785863a0580d52695a3281ebaf2fe4e
leewi9 <leewi9@yahoo.com>
2018-01-31 13:20:20 +0800

    Create fishshell.sh

---
+++ fishshell.sh
@@ -0,0 +1,9 @@
+#
+set -U fish_user_paths /usr/local/sbin $fish_user_paths
+set -U fish_user_paths /Users/leewi9/Desktop/_A_DEV/_A_CODE/_C_JAVA/_A_TOOLS/apache-maven-3.5.2/bin $fish_user_paths
+
+#
+set -Ux HTTPS_PROXY http://127.0.0.1:1087
+set -Ux HTTP_PROXY http://127.0.0.1:1087
+set -Ux https_proxy http://127.0.0.1:1087
+set -Ux http_proxy http://127.0.0.1:1087
