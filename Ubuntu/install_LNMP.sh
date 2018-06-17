7c9e8be1555e8be806f2a223d53b7d67c69eaf9d
leewi9 <leewi9@yahoo.com>
2018-06-17 15:21:19 +0800

    Update install_LNMP.sh

--- Ubuntu/install_LNMP.sh
+++ Ubuntu/install_LNMP.sh
@@ -21,13 +21,4 @@ sudo curl -sS https://getcomposer.org/installer | sudo php
 sudo mv composer.phar /usr/bin/composer
 sudo rm -f composer.phar 
 
-# 安装 mysql5.6 
-# 网上有说用这个ppa:ondrej/mysql-5.6，不要用！！
-# sudo add-apt-repository -y ppa:ondrej/mysql-5.6  # Don't use, it's broken.!!!
-# http://askubuntu.com/questions/690855/mysql-5-6-dpkg-install-error-even-after-complete-uninstall
-# 注意在vagrant中至少要分配1024内存，否则无法启动 failed to start
-echo "----> start installing mysql5.6 ..."
-sudo apt-get -y install debconf-utils
-sudo debconf-set-selections <<< 'mysql-server-5.6 mysql-server/root_password password 123456'  # 设置mysql的root密码！！
-sudo debconf-set-selections <<< 'mysql-server-5.6 mysql-server/root_password_again password 123456'
-sudo apt-get -y install mysql-server-5.6 mysql-client-5.6
+# 安装 mysql 见笔记
