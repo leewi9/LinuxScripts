03a75b6eab84402c3939db7082ba1d9adefc9732
leewi9 <leewi9@yahoo.com>
2018-06-17 15:42:19 +0800

    Update install_docker_ce.sh

--- Ubuntu/install_docker_ce.sh
+++ Ubuntu/install_docker_ce.sh
@@ -22,15 +22,3 @@ sudo apt-get install docker-ce
 #
 sudo usermod -a -G docker  $USER
 # 要logout才会生效
-
-#
-echo "DOCKER_OPTS=\"--registry-mirror=https://2h3po24q.mirror.aliyuncs.com\"" | sudo tee -a /etc/default/docker
-sudo service docker restart
-
-# 安装 phpmyadmin
-# docker run --name myadmin -d -e PMA_ARBITRARY=1 -p 8080:80 phpmyadmin/phpmyadmin
-
-
-# 安装 docker-compose
-sudo apt-get -y install python-pip
-sudo pip install docker-compose
