518721a1566ebc5a70580dfb0e25a032c52730ef
leewi9 <leewi9@PC>
2021-04-17 00:56:31 +0800

    update

--- ulimit_tuning.sh
+++ ulimit_tuning.sh
@@ -3,12 +3,12 @@
 # ulimit -a
 
 #
-copy /etc/systemd/system.conf /etc/systemd/system.conf.bak
+cp /etc/systemd/system.conf /etc/systemd/system.conf.bak
 echo 'DefaultLimitNOFILE=6553600' >> /etc/systemd/system.conf
 echo 'DefaultLimitNPROC=6553600'  >> /etc/systemd/system.conf
 
 #
-copy /etc/security/limits.conf /etc/security/limits.conf.bak
+cp /etc/security/limits.conf /etc/security/limits.conf.bak
 echo '* soft nofile 102400' >> /etc/security/limits.conf
 echo '* hard nofile 102400' >> /etc/security/limits.conf
 echo '* soft nproc 102400' >> /etc/security/limits.conf
