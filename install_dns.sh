25b8a923a889cd4fe9fbfe6fa46c03410e94625c
leewi9 <leewi9@yahoo.com>
2023-01-10 19:23:51 +0800

    Update install_dns.sh

--- install_dns.sh
+++ install_dns.sh
@@ -5,7 +5,7 @@
 sudo apt install resolvconf
 sudo systemctl enable --now resolvconf.service
 
-sudo echo 'nameserver 8.8.8.8' >> /etc/resolvconf/resolv.conf.d/head
-sudo echo 'nameserver 1.1.1.1' >> /etc/resolvconf/resolv.conf.d/head
+sudo bash -c 'echo "nameserver 8.8.8.8" >> /etc/resolvconf/resolv.conf.d/head'
+sudo bash -c 'echo "nameserver 1.1.1.1" >> /etc/resolvconf/resolv.conf.d/head'
 
 sudo resolvconf -u
