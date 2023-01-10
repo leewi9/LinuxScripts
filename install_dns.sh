bf53afa713eebaf4e8de05a6c02f3c2d4c413b08
leewi9 <leewi9@yahoo.com>
2023-01-10 19:25:00 +0800

    Update install_dns.sh

--- install_dns.sh
+++ install_dns.sh
@@ -9,3 +9,5 @@ sudo bash -c 'echo "nameserver 8.8.8.8" >> /etc/resolvconf/resolv.conf.d/head'
 sudo bash -c 'echo "nameserver 1.1.1.1" >> /etc/resolvconf/resolv.conf.d/head'
 
 sudo resolvconf -u
+
+dig www.yahoo.com
