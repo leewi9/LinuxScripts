bb9a49295465c8070e31fb3df344f9aca4a0c9a2
leewi9 <leewi9@yahoo.com>
2023-08-26 17:02:00 +0800

    Update ulimit_tuning.sh

--- ulimit_tuning.sh
+++ ulimit_tuning.sh
@@ -1,20 +1,13 @@
-# ubuntu 18.04
-
 # ulimit -a
 
-#
 cp /etc/systemd/system.conf /etc/systemd/system.conf.bak
 echo 'DefaultLimitNOFILE=6553600' >> /etc/systemd/system.conf
 echo 'DefaultLimitNPROC=6553600'  >> /etc/systemd/system.conf
 
-#
 cp /etc/security/limits.conf /etc/security/limits.conf.bak
 echo '* soft nofile 102400' >> /etc/security/limits.conf
 echo '* hard nofile 102400' >> /etc/security/limits.conf
 echo '* soft nproc 102400' >> /etc/security/limits.conf
 echo '* hard nproc 102400' >> /etc/security/limits.conf
-
-# 在 /etc/pam.d/common-session 文件中添加下面内容：
-# session required pam_limits.so
 
 # reboot
