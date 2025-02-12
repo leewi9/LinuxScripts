332ea53c476dcac28479321f99fb7edb52b837a5
leewi9 <leewi9@yahoo.com>
2025-02-12 14:35:40 +0800

    Update and rename debain.md to pyenv.sh

--- pyenv.sh
+++ pyenv.sh
@@ -1,4 +1,5 @@
 
+# Debian 12
 sudo apt-get install -y bzip2 libreadline-dev openssl make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev
 
 pyenv install 3.10.16
