6b632ad40eba1a3793528bd1e0152486234cfc60
leewi9 <leewi9@yahoo.com>
2025-02-12 14:40:52 +0800

    Create alias.sh

---
+++ alias.sh
@@ -0,0 +1,9 @@
+echo "" >> ~/.bashrc
+echo "# alias" >> ~/.bashrc
+echo "alias ll='ls -alFh'" >> ~/.bashrc
+echo "alias la='ls -alFh'" >> ~/.bashrc
+echo "alias l='ls -alFh'" >> ~/.bashrc
+
+echo "alias h='history'" >> ~/.bashrc
+echo "alias hg='history | grep '" >> ~/.bashrc
+echo "alias pg='ps -ef | grep '" >> ~/.bashrc
